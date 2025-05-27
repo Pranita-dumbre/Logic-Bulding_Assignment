17d7864c8b16c3966a4632c24349c36c8fd76dfa
Pranita-dumbre <pranitadumbre1885@gmail.com>
2025-05-27 18:22:58 +0530

    Add files via upload

---
+++ Assignment15/question15_3.c
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////
+//
+//  File name :   question15_3.c
+//  Discreption : Used to Display Pattern
+//  Author :      Pranita Purushottam Dumbre
+//  Date :        22/05/2025
+//
+///////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid input");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(j == i)
+            {
+                printf("$\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n"); 
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns: \n");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
